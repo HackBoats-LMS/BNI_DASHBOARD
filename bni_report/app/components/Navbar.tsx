8eeda3173140b3e938214953f2c8286dc0b96320
Akhil <akhilrajuvysyaraju19@gmail.com>
2026-05-27 10:59:52 +0530

    changes int he nav bar padding

--- bni_report/app/components/Navbar.tsx
+++ bni_report/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = ({ chapterData }: { chapterData?: any }) => {
     return (
-        <div className='w-full px-4 border-2 border-b-emerald-100  md:px-10 lg:px-20 bg-white flex justify-between items-center'>
+        <div className='w-full px-4 py-4 border-2 border-b-emerald-100  md:px-10 lg:px-20 bg-white flex justify-between items-center'>
             <div className='flex items-center'>
                 <span className='text-3xl font-black tracking-tight text-emerald-900 font-sans'>Impact</span>
                 <span className='text-3xl font-semibold tracking-tight text-emerald-600 font-sans'>Sync</span>
